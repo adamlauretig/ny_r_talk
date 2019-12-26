eea2b93eb8ec4054272fcaa94b16a716e68d9bbd
adamlauretig <lauretig.1@buckeyemail.osu.edu>
2019-12-26 00:40:13 -0500

    save model objects

--- fit_fm_model_2.R
+++ fit_fm_model_2.R
@@ -14,13 +14,12 @@ m1_fit <- sampling(m1, data = fm_simulation_1[[1]],
 # posterior predictive check
 m1_posterior <- extract(m1_fit)
 plot(fm_simulation_1[[1]]$y, colMeans(m1_posterior$y_pred))
-ppc_dens_overlay(y = fm_simulation_1[[1]]$y, yrep = m1_posterior$y_pred[seq(1, 4000, 5), ])
-
+p <- ppc_dens_overlay(y = fm_simulation_1[[1]]$y, yrep = m1_posterior$y_pred[seq(1, 4000, 10), ])
 
 
 fm_simulation_2[[1]]$gamma_sigma_prior <- 1
 fm_simulation_2[[1]]$delta_sigma_prior <- 1
-m2_fit <- vb(m1, data = fm_simulation_2[[1]], seed = 216, tol_rel_obj = 1e-3
+m2_fit <- vb(m1, data = fm_simulation_2[[1]], seed = 216, tol_rel_obj = 1e-3, importance_resampling = TRUE
 )
 
 m2_fit <- sampling(m1, data = fm_simulation_2[[1]], 
@@ -30,4 +29,6 @@ m2_fit <- sampling(m1, data = fm_simulation_2[[1]],
 
 # posterior predictive check
 m2_posterior <- extract(m2_fit)
-ppc_dens_overlay(y = fm_simulation_2[[1]]$y, yrep = m2_posterior$y_pred[seq(1, 1000, 5), ])
+ppc_dens_overlay(y = fm_simulation_2[[1]]$y, yrep = m2_posterior$y_pred[seq(1, 4000, 10), ])
+
+save(m1_fit, m2_fit, file = "hierarchical_fm.rdata")
